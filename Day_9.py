25136530344873d6718d3a7cc48d82cd851a3e8c
MohammadUmar93 <142153486+MohammadUmar93@users.noreply.github.com>
2023-08-15 09:54:41 +0530

    Create Day_9.py

---
+++ Day_9.py
@@ -0,0 +1,10 @@
+# Day_9 : Reverse a string using recursion.
+def reverse_string(s):
+    if len(s) == 0:
+        return s
+    else:
+        return reverse_string(s[1:]) + s[0]
+
+input_string = "Hello World"
+reversed_string = reverse_string(input_string)
+print(reversed_string)
